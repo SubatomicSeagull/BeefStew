95570cbd0f969c83b8f26c574b4a757181400bde
SubatomicSeagull <subatomicseagull@gmail.com>
2024-09-21 19:51:36 +0100

    assets

--- responses.py
+++ responses.py
@@ -13,6 +13,9 @@ def get_response(message):
         response = "🎲You my friend... have just made.. an.. unlucky gamble...🎲\nhttps://cdn.discordapp.com/attachments/1185154272170623016/1216308263348605049/17100608047072368967661815628.gif?ex=66ee89d7&is=66ed3857&hm=a331b00d199db639dadec8dfd406cb7bf1027c7d968ffcb532ba6cb5954badd4&"
     elif "preposition" in message_text:
         response = "prepositions\nhttps://www.youtube.com/watch?v=E0Ag0VKJ51Y"
+    elif message_text == "reveal the banger":
+        response = "https://x.com/blacklung82/status/1602348576331505664"
+        
         
     #check for emoji responses
     if "whatsapp" in message_text:
@@ -23,5 +26,7 @@ def get_response(message):
         response = response + "<:romulus:1285630098224971786>"
     if "brazil" in message_text:
         response = response + "<:brazil:1286688098297839696>"
+    if "elphaba" in message_text:
+        response = response + "<:beefhusk:1287094093989679124>"
         
     return response
