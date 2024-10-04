d19df464e4ac020631cea0f9244a94fd3ccff9a1
mertymor <mertymor@gmail.com>
2024-10-04 13:16:04 +0300

    refactor response handling logic and add new responses

--- src/responses.py
+++ src/responses.py
@@ -1,33 +1,45 @@
 def get_response(message):
     message_text = str(message).lower()
+    if not message_text:
+        return ""
+
+    # Dictionary for text responses
+    text_responses = {
+        "oye": "oye.",
+        "snarf": "snarf >:(",
+        "reveal the banger": "https://x.com/blacklung82/status/1602348576331505664"
+    }
+
+    # Dictionary for keyword responses
+    keyword_responses = {
+        "deadly dice man": "🎲You my friend... have just made.. an.. unlucky gamble...🎲\nhttps://cdn.discordapp.com/attachments/1185154272170623016/1216308263348605049/17100608047072368967661815628.gif?ex=66ee89d7&is=66ed3857&hm=a331b00d199db639dadec8dfd406cb7bf1027c7d968ffcb532ba6cb5954badd4&",
+        "preposition": "prepositions\nhttps://www.youtube.com/watch?v=E0Ag0VKJ51Y"
+    }
+
+    # Dictionary for emoji responses
+    emoji_responses = {
+        "whatsapp": "<:whatsapp:1285617179408076902>",
+        "beefstew": "<:beefstew:1285630081829437614>",
+        "romulus": "<:romulus:1285630098224971786>",
+        "brazil": "<:brazil:1286688098297839696>",
+        "elphaba": "<:beefhusk:1287094093989679124>"
+    }
+
     response = ""
 
+    # Check for exact text responses
+    if message_text in text_responses:
+        response += text_responses[message_text]
 
-    #check for text responses
-    if message_text == "":
-        return
-    elif message_text == "oye":
-        response = "oye."
-    elif message_text == "snarf":
-        response = "snarf >:("
-    elif "deadly dice man" in message_text:
-        response = "🎲You my friend... have just made.. an.. unlucky gamble...🎲\nhttps://cdn.discordapp.com/attachments/1185154272170623016/1216308263348605049/17100608047072368967661815628.gif?ex=66ee89d7&is=66ed3857&hm=a331b00d199db639dadec8dfd406cb7bf1027c7d968ffcb532ba6cb5954badd4&"
-    elif "preposition" in message_text:
-        response = "prepositions\nhttps://www.youtube.com/watch?v=E0Ag0VKJ51Y"
-    elif message_text == "reveal the banger":
-        response = "https://x.com/blacklung82/status/1602348576331505664"
+    # Check for keyword responses
+    for keyword, keyword_response in keyword_responses.items():
+        if keyword in message_text:
+            response += keyword_response
+            break
 
-
-    #check for emoji responses
-    if "whatsapp" in message_text:
-        response = response + "<:whatsapp:1285617179408076902>"
-    if "beefstew" in message_text:
-        response = response + "<:beefstew:1285630081829437614>"
-    if "romulus" in message_text:
-        response = response + "<:romulus:1285630098224971786>"
-    if "brazil" in message_text:
-        response = response + "<:brazil:1286688098297839696>"
-    if "elphaba" in message_text:
-        response = response + "<:beefhusk:1287094093989679124>"
+    # Append emoji responses in order of appearance
+    for word in message_text.split():
+        if word in emoji_responses:
+            response += emoji_responses[word]
 
     return response
