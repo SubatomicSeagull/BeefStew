8e5740c92e0e634241bc5799254312057b58e8be
SubatomicSeagull <subatomicseagull@gmail.com>
2025-03-07 18:08:31 +0000

    oops

--- patchnotes.md
+++ patchnotes.md
@@ -1,21 +1,12 @@
 # BEEFSTEW has been updated!
 
-## Beefstew version 1.4.1 patchnotes:
+## Beefstew version 1.4.3 patchnotes:
 
 ## Features:
-- added more trigger phrases, see if you can find them!
-- new kay is better :)
 
 ## Fixes:
-- fixed an issue where trigger phrases weren't finding their media properly
-- fixed the message edit event after breaking it in the last updat
-- removed a command from the bot tree that was causing problems 
-- fixed a typo in the logging where -2 was actaully +2
+- fixed the pointer to the responses, and added some new ones
 
 ## Misc:
-- added the ccserver icon to ccping command
-- reformatted some of the visage commands to look a bit nicer
-- added the framework for a task scheduler
 
-i have some fixes for the music player coming its just reading through the spaghetti code and trying to fit them in lol.
-theres some fun stuff in the works tho :)
+sorry
--- src/beefcommands/invocations/joker_score/leaderboard.py
+++ src/beefcommands/invocations/joker_score/leaderboard.py
@@ -16,7 +16,10 @@ async def retrive_top_scores(interaction: discord.Interaction, bot):
     else:
         leaderboard_content = ""
         for rank, row in enumerate(rows, start=1):
-            leaderboard_content += f"**{rank}.** <@{row[0]}> - `{row[1]}` points\n"
+            if row[0] == 99:
+                leaderboard_content += f"**{rank}.** Hawk Tuah Jar: `{row[1]}` points\n"
+            else:
+                leaderboard_content += f"**{rank}.** <@{row[0]}>: `{row[1]}` points\n"
         leaderboard.description = leaderboard_content
     await interaction.followup.send(embed=leaderboard)
     
@@ -26,7 +29,7 @@ async def retrive_low_scores(interaction: discord.Interaction, bot):
     rows = await postgres.read("SELECT user_id, joke_score FROM user_joker_score ORDER BY joke_score ASC LIMIT 10")
     
     # embed header
-    leaderboard = discord.Embed(title= "# Joke Score Loserboard", color=discord.Color.fuchsia())
+    leaderboard = discord.Embed(title= "Joke Score Loserboard", color=discord.Color.fuchsia())
     leaderboard.set_author(name="Beefstew", icon_url=bot.user.avatar.url)
 
     # read the rows returned from the sql query and add them to the embed body
