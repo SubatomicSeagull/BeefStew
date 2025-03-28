b4e23f9f82412c4856b51d1b31d66e67ed4810a1
SubatomicSeagull <subatomicseagull@gmail.com>
2025-03-28 14:52:07 +0000

    /sniff command & unsniff

--- src/beefcommands/visage/sniff.py
+++ src/beefcommands/visage/sniff.py
@@ -9,12 +9,10 @@ from data import postgres
 async def sniff_overlay(victim: discord.Member):
     #retrive the users pfp and resize
     avatar = await get_avatar_image(victim)
-    new_size = (int(avatar.width * 0.5), int(avatar.height * 0.5))
+    new_size = (int(avatar.width * 2), int(avatar.height * 2))
     user_pfp = avatar.resize(new_size, Image.Resampling.BILINEAR)
     
     # construct a file path to the assets folder
-    current_dir = os.path.dirname(__file__)
-    file_path = os.path.join(current_dir, '..', '..')
     template = Image.open(file_io.construct_assets_path("pfp_manipulation/beefsniff.png"))
 
     # resize the template
@@ -22,16 +20,16 @@ async def sniff_overlay(victim: discord.Member):
     overlay_width, overlay_height = template.size
 
     # define the overlay coordinates and size
-    x = (overlay_width - base_width) // 1 - 0
-    y = (overlay_height - base_height) // 1 - 0
+    x = (overlay_width - base_width) // 1 -700
+    y = (overlay_height - base_height) // 1 + 20
 
-    # compose the final image
-    final_image = Image.new("RGBA", template.size, (0, 0, 0))
+    # compose the new image
+    final_image = Image.new("RGBA", template.size, (255, 255, 255))
     final_image.paste(user_pfp, (x,y))
     final_image.paste(template, (0,0), template)
-    
-    final_image.show()
-    # write to the image binary buffer and return
+
+
+    # write the image binary buffer and return
     image_binary = BytesIO()
     final_image.save(image_binary, 'PNG')
     image_binary.seek(0)
@@ -42,10 +40,10 @@ async def sniff_user(interaction: discord.Interaction, victim: discord.Member):
     await interaction.response.defer()
     try:
         drain_pfp = await sniff_overlay(victim)
-        await interaction.followup.send(content= f"sniff", file=discord.File(fp=drain_pfp, filename=f"{victim.name}_sniff.png"))
+        await interaction.followup.send(content= f"Beefstew will remember your scent...", file=discord.File(fp=drain_pfp, filename=f"{victim.name}_sniff.png"))
         
         # clear the bytesio buffer
         drain_pfp.close()
     except Exception as e:
         postgres.log_error(e)
-        await interaction.followup.send(f"{interaction.user.mention} tried to drop {victim.name} down the drain but it didnt work :// ({e})")
+        await interaction.followup.send(f"{interaction.user.mention} tried to sniff {victim.name} but it didnt work :// ({e})")
