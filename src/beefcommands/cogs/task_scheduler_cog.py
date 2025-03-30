63a133817aa15591334ed809ea9ca2f5202b1209
SubatomicSeagull <subatomicseagull@gmail.com>
2025-03-30 18:56:30 +0100

    add random swing functionality and integrate with task scheduler

--- src/beefcommands/cogs/task_scheduler_cog.py
+++ src/beefcommands/cogs/task_scheduler_cog.py
@@ -5,7 +5,7 @@ import datetime
 from data import postgres
 import random
 from zoneinfo import ZoneInfo
-from beefcommands.events.tasks import holiday_check, birthday_check, image_of_the_day
+from beefcommands.events.tasks import holiday_check, birthday_check, image_of_the_day, random_swing
 class TaskSchedulerCog(commands.Cog):
     
     TIMEZONE = ZoneInfo("Europe/London")
@@ -16,6 +16,8 @@ class TaskSchedulerCog(commands.Cog):
         print(f"> Scheduling Tasks...")
         self.scheduled_birthday_check.start()
         self.scheduled_holiday_check.start()
+        self.image_of_the_day_check.start()
+        self.random_swing_check.start()
         print(f"\033[32mall tasks scheduled successfully!\033[0m")
     
     # task is scheduled to check for birthdays every day at 10am
@@ -34,39 +36,14 @@ class TaskSchedulerCog(commands.Cog):
         await image_of_the_day.image_of_the_day(self.bot)
         return
 
-
-    
-    @tasks.loop(hours=24)
+    @tasks.loop(time=[
+        datetime.time(hour=15, minute=55, tzinfo=TIMEZONE),
+        datetime.time(hour=14, minute=30, tzinfo=TIMEZONE),
+        datetime.time(hour=14, minute=35, tzinfo=TIMEZONE),
+        datetime.time(hour=14, minute=36, tzinfo=TIMEZONE)
+    ])
     async def random_swing_check(self):
-        swing_times = [8, 12, 16, 20]  # 8am, 12pm, 4pm, 8pm
-        for hour in swing_times:
-            
-            # add a random number of minutes to the hour
-            random_minutes = random.randint(0, 59)
-            swing_time = datetime.time(hour, random_minutes, tzinfo=self.TIMEZONE)
-            now = datetime.datetime.now(tz=self.TIMEZONE).time()
-            print(f"Checking for swing at {swing_time} against {now}")
-            # check if the current time matches the swing time
-            if now.hour != swing_time.hour and now.minute != swing_time.minute:
-                # 1/100 chance for the swing to happen
-                if random.randint(1, 100) != 1:
-                    
-                    # determine the type of swing
-                    swing_type = random.choice(["insult", "neutral", "image"])
-                    
-                    # determine the channel type
-                    channel_type = random.choices(["info_channel", "dm"], weights=[1, 2])[0]
-
-                    # read flagged users from the db
-                    flagged_users = await postgres.read(f"SELECT user_id FROM users WHERE msg_flag = TRUE;")
-                    if not flagged_users:
-                        return
-                    print(f"Flagged users: {flagged_users}")
-                    
-                    # pick a random user
-                    user_id = random.choice(flagged_users)
-                    
-                    print(f"Swinging at user {user_id} with type {swing_type} in {channel_type}")
+        await random_swing.random_swing_check(self.bot)
 
 async def setup(bot):
     print("- \033[95mbeefcommands.cogs.task_scheduler_cog\033[0m")
